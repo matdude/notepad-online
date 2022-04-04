52a256d4f7c4179c245de645342ca8cad0cdc7ce
Mateusz Dudek <mateusz.dudek03@gmail.com>
2022-04-04 14:19:13 +0200

    First commit

---
+++ src/main/java/com/project/notepad_online/NotepadOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.project.notepad_online;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotepadOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotepadOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/notepad_online/NotepadOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.notepad_online;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotepadOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
